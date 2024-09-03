1f2002a6debe6c4e9655537757eb5b89bdc6b764
Ansalpandey <pandeyansal77@gmail.com>
2024-09-03 08:59:14 +0530

    profile response added

---
+++ app/src/main/java/com/example/project_x/data/model/ProfileResponse.kt
@@ -0,0 +1,6 @@
+package com.example.project_x.data.model
+
+data class ProfileResponse(
+  val message: String?, // User profile retrieved successfully!
+  val user: UserX?,
+)
---
+++ app/src/main/java/com/example/project_x/data/model/UserX.kt
@@ -0,0 +1,23 @@
+package com.example.project_x.data.model
+
+data class UserX(
+  val __v: Int?, // 1
+  val _id: String?, // 669a69c129a4e7da7baa72c2
+  val age: Int?, // 21
+  val bio: String?, // this is my bio
+  val courses: List<Course?>?,
+  val createdAt: String?, // 2024-07-19T13:27:29.415Z
+  val email: String?, // rohit@gmail.com
+  val enrolledCourses: List<EnrolledCourse?>?,
+  val location: String?,
+  val followers: List<Follower>?,
+  val followersCount: Int?, // 0
+  val following: List<FollowingResponse?>?,
+  val followingCount: Int?, // 0
+  val name: String?, // Rohit
+  val password: String?, // $2b$10$m2SjycPrjrKTnAj18U1XqeX3YAIo5yRRt8z9PC3dXHcQyLEQyU72a
+  val posts: List<Post?>?,
+  val profileImage: String?,
+  val updatedAt: String?, // 2024-07-19T13:28:41.524Z
+  val username: String?, // rohit
+)
